5a5d6a38677a128dfe039f2f8b4bbd1547d39dee
agathadantas <agathadantasmagro@hotmail.com>
2019-10-29 22:04:12 -0300

    adicionando comentários de funcionalidades da estrutura MVC

--- src/controller/tarefasController.js.js
+++ src/controller/tarefasController.js.js
@@ -1,3 +1,13 @@
+/*É a camada de controle.
+Exerce o controle de qual model deverá ser aplicado e qual view será mostrado ao usuário.
+Podemos dizer que esta camada faz uma gerência das outras duas camadas.
+O controller manipula e roteia as requisições dos usuários.
+Interpreta as requisições submetidas pelo usuário e traduz em comandos que são enviados 
+para o (Model) e/ou para a View) .
+Valida as requisições dos usuários de acordo com as regras de autenticação e autorização.*/
+
+
+
 var Controller = {
     callback : function(){
          
---
+++ src/routes/tarefasRoute.js
@@ -0,0 +1,6 @@
+/*É a camada que exibe uma representação dos dados.
+É camada de interface com usuário (view).
+Também conhecida como cliente-side.
+Faz a exibição dos dados, utilizando-se de #HTML e/ou XML.
+É responsável por usar as informações modeladas para produzir 
+interfaces de apresentação conforme a necessidade.*/
